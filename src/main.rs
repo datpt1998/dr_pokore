ef1ee6204052b12c708b7156ec1d204399204715
datpt <datpt@timebird.org>
2023-02-14 17:03:17 +0700

    change to_string_decks to more dynamic

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,9 @@ fn main() {
     let deck_1 = poker_match.draw_player().unwrap().into_iter().map(|card| Rc::new(card)).collect::<Vec<_>>();
     let deck_2 = poker_match.draw_player().unwrap().into_iter().map(|card| Rc::new(card)).collect::<Vec<_>>();
     let deck_general = poker_match.draw_general().unwrap().into_iter().map(|card| Rc::new(card)).collect::<Vec<_>>();
-    println!("Player 1: {}", to_string_decks(&deck_1));
-    println!("Player 2: {}", to_string_decks(&deck_2));
-    println!("General: {}", to_string_decks(&deck_general));
+    println!("Player 1: {}", to_string_decks(deck_1.iter().map(|card| Rc::clone(card)).collect()));
+    println!("Player 2: {}", to_string_decks(deck_2.iter().map(|card| Rc::clone(card)).collect()));
+    println!("General: {}", to_string_decks(deck_general.iter().map(|card| Rc::clone(card)).collect()));
 
     let (score_1, chosen_1, type_1) = score_by_two_deck(&deck_1, &deck_general).unwrap();
     let (score_2, chosen_2, type_2) = score_by_two_deck(&deck_2, &deck_general).unwrap();
--- src/util/poker_util.rs
+++ src/util/poker_util.rs
@@ -1,7 +1,8 @@
+use std::ops::Deref;
 use std::rc::Rc;
 use crate::entity::card::PokerCard;
 
-pub fn to_string_decks(decks : &Vec<Rc<PokerCard>>) -> String {
+pub fn to_string_decks(decks : Vec<impl Deref<Target = PokerCard>>) -> String {
     let mut result = String::new();
     for (index, card) in decks.iter().enumerate() {
         result.push_str(card.to_string().as_str());
